3b59587078f2299f13ae670ad33097b4de082257
rivanda m <mahdiyansyahr@gmail.com>
2020-11-04 09:48:10 +0700

    changes

--- app/src/main/java/com/rivaphy/stuper/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/rivaphy/stuper/onboarding/OnBoardingActivity.kt
@@ -3,6 +3,8 @@ package com.rivaphy.stuper.onboarding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.viewpager.widget.ViewPager
 import com.rivaphy.stuper.MainActivity
 import com.rivaphy.stuper.R
 import kotlinx.android.synthetic.main.activity_on_boarding.*
@@ -32,5 +34,47 @@ class OnBoardingActivity : AppCompatActivity() {
         btn_on_boarding.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
+        btn_next_on_boarding.setOnClickListener {
+            if (vp_on_boarding.currentItem != vp_on_boarding.adapter!!.count - 1 ) {
+                vp_on_boarding.currentItem = vp_on_boarding.currentItem + 1
+            }
+        }
+
+        btn_skip_on_boarding.setOnClickListener {
+            if (vp_on_boarding.currentItem == 0) {
+                vp_on_boarding.currentItem = vp_on_boarding.currentItem + 2
+            } else {
+                vp_on_boarding.currentItem = vp_on_boarding.currentItem + 1
+            }
+        }
+
+        setPageChange()
+    }
+
+    private fun setPageChange() {
+
+        vp_on_boarding.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                if (vp_on_boarding.currentItem == vp_on_boarding.adapter!!.count - 1) {
+                    btn_on_boarding.visibility = View.VISIBLE
+                    btn_next_on_boarding.visibility = View.INVISIBLE
+                    btn_skip_on_boarding.visibility = View.INVISIBLE
+                } else {
+                    btn_on_boarding.visibility = View.INVISIBLE
+                }
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+
+        })
     }
 }
